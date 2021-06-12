9139085374d6738377b4f0ea99a729bfc0516853
Francisco Puma <francis_pancho12@hotmail.com>
2021-06-12 10:34:30 -0700

    included required packages

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatedPage = require('./src/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
