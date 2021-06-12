c35a7e1749c7fe657c98a6256415ad0580ec864d
Francisco Puma <francis_pancho12@hotmail.com>
2021-06-12 10:47:31 -0700

    scoping out the questions

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const generatedPage = require('./src/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+    //Title
+        // the title of my project
+    // First Sections 
+        // Description, 
+        // Installation, 
+        // Usage, 
+        // Contributing
+        // Tests
+    // License  
+        //a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under)
+    // Questions
+        // GitHub username
+            // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+        // Email address
+            // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
